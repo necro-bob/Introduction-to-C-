00b6f4f852d7f1a684ccd10df605ebcc66d5254f
Robert Shell <40207153+necro-bob@users.noreply.github.com>
2018-06-25 17:14:04 -0500

    Complete CSC101 Introduction to C#

---
+++ CSC101/Retirementapp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Retirementapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int retirement = 65;
+
+            int myAge = 25;
+            int yourAge = 35;
+
+            string message = "Will we be ready to retire soon?";
+
+            int yearsToRetirementForYou = retirement - yourAge;
+            int yearsToRetirementForme = retirement - myAge;
+
+            Console.WriteLine(message);
+
+            Console.WriteLine("Years for you: " + yearsToRetirementForYou);
+            Console.WriteLine("Years for me: {0}", yearsToRetirementForme);
+        }
+    }
+}
